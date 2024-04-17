7da3335971da5bfd60422f54db3ad2dc7763038e
georgenebieridze <giorgi.nebieridze.gn@gmail.com>
2024-04-16 18:13:22 -0700

    Updated Tides

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/services/TidesQueryService.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/TidesQueryService.java
@@ -1,7 +1,5 @@
 package edu.ucsb.cs156.spring.backenddemo.services;
 
-
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.web.client.RestTemplate;
@@ -25,7 +23,7 @@ public class TidesQueryService {
     }
 
     // Documentation for endpoint is at: https://api.tidesandcurrents.noaa.gov/api/prod/
-    public static final String ENDPOINT = "";
+    public static final String ENDPOINT = "https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?application=ucsb-cs156&begin_date={beginDate}&end_date={endDate}&station={station}&product=predictions&datum=mllw&units=english&time_zone=lst_ldt&interval=hilo&format=json";
 
     public String getJSON(String beginDate, String endDate, String station) throws HttpClientErrorException {
         return "";
